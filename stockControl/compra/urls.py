f686c7ef25e0803a03dc04409bcd6b6cb93d048e
Pablo Matias <pmpidone@yahoo.com.ar>
2024-04-07 02:41:38 -0300

    Trabajo Final se suman modelos, vistas, formularios para la carga y edición de Proveedores y Productos

--- stockControl/compra/urls.py
+++ stockControl/compra/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('proveedores', views.lista_proveedores, name='lista_proveedores'),
     path('productos', views.lista_productos, name='lista_productos'),
+    path('agregar_proveedor', views.agregar_proveedor, name='agregar_proveedor'),
+    path('agregar_producto', views.agregar_producto, name='agregar_producto'),
+    path('editar_proveedor/<int:proveedor_id>', views.editar_proveedor, name='editar_proveedor'),
+    path('editar_producto/<int:producto_id>', views.editar_producto, name='editar_producto'),
 ]
--- stockControl/compra/views.py
+++ stockControl/compra/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Proveedor, Producto
+from .forms import ProveedorForm, ProductoForm
 
 def lista_proveedores(request):
     proveedores = Proveedor.objects.all()
@@ -8,3 +9,45 @@ def lista_proveedores(request):
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'lista_productos.html', {'productos': productos})
+
+def agregar_proveedor(request):
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_proveedores')
+    else:
+        form = ProveedorForm()
+    return render(request, 'agregar_proveedor.html', {'form': form})
+
+def agregar_producto(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm()
+    return render(request, 'agregar_producto.html', {'form': form})
+
+def editar_proveedor(request, proveedor_id):
+    proveedor = get_object_or_404(Proveedor, pk=proveedor_id)
+    if request.method == 'POST':
+        form = ProveedorForm(request.POST, instance=proveedor)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_proveedores')
+    else:
+        form = ProveedorForm(instance=proveedor)
+    return render(request, 'editar_proveedor.html', {'form': form})
+
+def editar_producto(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_productos')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'editar_producto.html', {'form': form})
